b6651c69e93897f7d37a75ab7f3d53d8323b51a3
Giovana G. Silva <giovanagon22@yahoo.com>
2026-07-29 11:31:11 -0300

    Gerador de conjuntos CSV de eventos - update
    CÓDIGO ALTERNATIVO: Todos os atributos de um evento em uma mesma coluna no formato string

--- gerador_conjuntosCSV.py
+++ gerador_conjuntosCSV.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 df = pd.read_csv("eventosTratados.csv", sep=";") # Mudar nome do arquivo
+df["ANO-DO-EVENTO"] = df["ANO-DO-EVENTO"].astype("Int64")
 
 # PRIMEIRO CONJUNTO: NOME-DO-EVENTO
-df[["NOME-DO-EVENTO"]].to_csv("conjunto1.csv", sep="|",index=False)
+df["NOME-DO-EVENTO"].to_csv("conjunto1.csv", sep="|",index=False)
 
 # SEGUNDO CONJUNTO: NOME-DO-EVENTO | ANO-DO-EVENTO
 df[
@@ -31,3 +32,29 @@ df[
         "CIDADE-DO-EVENTO"
     ]
 ].to_csv("conjunto4.csv", sep="|",index=False)
+
+
+'''
+CÓDIGO ALTERNATIVO: Todos os atributos em uma só coluna
+
+# SEGUNDO CONJUNTO: NOME-DO-EVENTO | ANO-DO-EVENTO
+df["NOME-DO-EVENTO|ANO-DO-EVENTO"] = (
+    df["NOME-DO-EVENTO"] + "|" + 
+    df["ANO-DO-EVENTO"].astype("Int64").astype(str)
+)
+df["NOME-DO-EVENTO|ANO-DO-EVENTO"].to_csv("conjunto2.csv", sep="|",index=False)
+
+# TERCEIRO CONJUNTO: NOME-DO-EVENTO | ANO-DO-EVENTO | PAIS-DO-EVENTO
+df["NOME-DO-EVENTO|ANO-DO-EVENTO|PAIS-DO-EVENTO"] = (
+    df["NOME-DO-EVENTO|ANO-DO-EVENTO"] + "|" +
+    df["PAIS-DO-EVENTO"]
+)
+df["NOME-DO-EVENTO|ANO-DO-EVENTO|PAIS-DO-EVENTO"].to_csv("conjunto3.csv", sep="|",index=False)
+
+# QUARTO CONJUNTO: NOME-DO-EVENTO | ANO-DO-EVENTO | PAIS-DO-EVENTO | CIDADE-DO-EVENTO
+df["NOME-DO-EVENTO|ANO-DO-EVENTO|PAIS-DO-EVENTO|CIDADE-DO-EVENTO"] = (
+    df["NOME-DO-EVENTO|ANO-DO-EVENTO|PAIS-DO-EVENTO"] + "|" +
+    df["CIDADE-DO-EVENTO"]
+)
+df["NOME-DO-EVENTO|ANO-DO-EVENTO|PAIS-DO-EVENTO|CIDADE-DO-EVENTO"].to_csv("conjunto4.csv", sep="|",index=False)
+'''
