8d938e240fe75d53ff8d5f638574ea0da323acda
Giovana G. Silva <giovanagon22@yahoo.com>
2026-08-20 18:12:52 -0300

    ATUALIZAÇÃO - quantificador de eventos
    Cria 4 arquivos .csv de eventos quantificados e organizados decrescentemente por meio dos seguintes parâmetros:
    -> NOME-DO-EVENTO
    -> NOME-DO-EVENTO, ANO-DO-EVENTO
    -> NOME-DO-EVENTO, CIDADE-DO-EVENTO, ANO-DO-EVENTO
    -> NOME-DO-EVENTO, PAIS-DO-EVENTO, ANO-DO-EVENTO

--- quant_ocorrencias.py
+++ quant_ocorrencias.py
@@ -6,9 +6,10 @@ import time
 csvEntrada = r''
 csvSaida = r''
 
-csvConjunto1_ordenado = os.path.join(csvSaida, 'conjunto1_ordenado.csv')
-csvConjunto2_ordenado = os.path.join(csvSaida, 'conjunto2_ordenado.csv')
-csvConjunto3_ordenado = os.path.join(csvSaida, 'conjunto3_ordenado.csv')
+csvConjunto1_ordenado = os.path.join(csvSaida, 'conjuntoN_ordenado.csv')
+csvConjunto2_ordenado = os.path.join(csvSaida, 'conjuntoNA_ordenado.csv')
+csvConjunto3_ordenado = os.path.join(csvSaida, 'conjuntoNCA_ordenado.csv')
+csvConjunto4_ordenado = os.path.join(csvSaida, 'conjuntoNPA_ordenado.csv')
 
 intervaloFlush = 300000
 
@@ -19,48 +20,61 @@ def main():
     totalLinhas1 = 0
     totalLinhas2 = 0
     totalLinhas3 = 0
+    totalLinhas4 = 0
 
+    # Criação dos quantificadores
     frequencia_evento = Counter()
-    frequencia_evento_pais = Counter()
-    frequencia_evento_cidade = Counter()
+    frequencia_evento_ano = Counter()
+    frequencia_evento_cidade_ano = Counter()
+    frequencia_evento_pais_ano = Counter()
 
+    # Abertura dos arquivos para leitura e escrita
     with open(csvEntrada, newline='', encoding='utf-8', mode='r') as f_entrada, \
         open(csvConjunto1_ordenado, newline='', encoding='utf-8', mode='w') as f1, \
         open(csvConjunto2_ordenado, newline='', encoding='utf-8', mode='w') as f2, \
-        open(csvConjunto3_ordenado, newline='', encoding='utf-8', mode='w') as f3:
+        open(csvConjunto3_ordenado, newline='', encoding='utf-8', mode='w') as f3, \
+        open(csvConjunto4_ordenado, newline='', encoding='utf-8', mode='w') as f4:
 
-        # Leitura e contagem das linhas como tuplas
         leitor = csv.DictReader(f_entrada, delimiter='|')
 
+        # Contagem de ocorrências
         for linha in leitor:
 
             totalLinhas += 1  
 
             nome = linha["NOME-DO-EVENTO"]
+            ano = linha["ANO-DO-EVENTO"]
             pais = linha["PAIS-DO-EVENTO"]
             cidade = linha["CIDADE-DO-EVENTO"]
     
             frequencia_evento[nome] += 1
-            frequencia_evento_pais[(nome, pais)] += 1
-            frequencia_evento_cidade[(nome, cidade)] += 1
+            frequencia_evento_ano[(nome, ano)] += 1
+            frequencia_evento_cidade_ano[(nome, cidade, ano)] += 1
+            frequencia_evento_pais_ano[(nome, pais, ano)] += 1
 
+        # Ordenação das ocorrências
         eventos_ordenados = sorted(
             frequencia_evento,
             key=frequencia_evento.get,
             reverse=True
         )
-        eventos_paises_ordenados = sorted(
-            frequencia_evento_pais,
-            key=frequencia_evento_pais.get,
+        evento_ano_ordenados = sorted(
+            frequencia_evento_ano,
+            key=frequencia_evento_ano.get,
             reverse=True
         )
-        eventos_cidades_ordenados = sorted(
-            frequencia_evento_cidade,
-            key=frequencia_evento_cidade.get,
+        evento_cidade_ano_ordenados = sorted(
+            frequencia_evento_cidade_ano,
+            key=frequencia_evento_cidade_ano.get,
+            reverse=True
+        )
+        evento_pais_ano_ordenados = sorted(
+            frequencia_evento_pais_ano,
+            key=frequencia_evento_pais_ano.get,
             reverse=True
         )
 
-        # Ordenação EVENTO
+        # CSV ordenado por frequência de EVENTO
         escritor = csv.DictWriter(f1, delimiter='|', fieldnames= ['NOME-DO-EVENTO', 'OCORRENCIAS-EVENTO'])
         escritor.writeheader()
 
@@ -75,45 +89,63 @@ def main():
             if totalLinhas1 % intervaloFlush == 0:
                 f1.flush()
 
-        # Ordenação EVENTO-PAÍS
-        escritor = csv.DictWriter(f2, delimiter='|', fieldnames= ['NOME-DO-EVENTO', 'PAIS-DO-EVENTO', 'OCORRENCIAS-EVENTO-PAIS'])
+        # CSV ordenado por frequência de EVENTO-ANO
+        escritor = csv.DictWriter(f2, delimiter='|', fieldnames= ['NOME-DO-EVENTO', 'ANO-DO-EVENTO', 'OCORRENCIAS-EVENTO-ANO'])
         escritor.writeheader()
 
-        for evento, pais in eventos_paises_ordenados:
+        for evento, ano in evento_ano_ordenados:
             escritor.writerow({
                 'NOME-DO-EVENTO': evento,
-                'PAIS-DO-EVENTO': pais,
-                'OCORRENCIAS-EVENTO-PAIS':
-                    frequencia_evento_pais[(evento, pais)]
+                'ANO-DO-EVENTO': ano,
+                'OCORRENCIAS-EVENTO-ANO':
+                    frequencia_evento_ano[(evento, ano)]
             })
             totalLinhas2 += 1
             
             if totalLinhas2 % intervaloFlush == 0:
                 f2.flush()
 
-        # Ordenação EVENTO-CIDADE
-        escritor = csv.DictWriter(f3, delimiter='|', fieldnames= ['NOME-DO-EVENTO', 'CIDADE-DO-EVENTO', 'OCORRENCIAS-EVENTO-CIDADE'])
+        # CSV ordenado por frequência de EVENTO-CIDADE-ANO
+        escritor = csv.DictWriter(f3, delimiter='|', fieldnames= ['NOME-DO-EVENTO', 'CIDADE-DO-EVENTO', 'ANO-DO-EVENTO', 'OCORRENCIAS-EVENTO-CIDADE-ANO'])
         escritor.writeheader()
 
-        for evento, cidade in eventos_cidades_ordenados:
+        for evento, cidade, ano in evento_cidade_ano_ordenados:
             escritor.writerow({
                 'NOME-DO-EVENTO': evento,
                 'CIDADE-DO-EVENTO': cidade,
-                'OCORRENCIAS-EVENTO-CIDADE':
-                    frequencia_evento_cidade[(evento, cidade)]
+                'ANO-DO-EVENTO': ano,
+                'OCORRENCIAS-EVENTO-CIDADE-ANO':
+                    frequencia_evento_cidade_ano[(evento, cidade, ano)]
             })
             totalLinhas3 += 1
             
             if totalLinhas3 % intervaloFlush == 0:
                 f3.flush()
+
+        # CSV ordenado por frequência de EVENTO-PAIS-ANO
+        escritor = csv.DictWriter(f4, delimiter='|', fieldnames= ['NOME-DO-EVENTO', 'PAIS-DO-EVENTO', 'ANO-DO-EVENTO', 'OCORRENCIAS-EVENTO-PAIS-ANO'])
+        escritor.writeheader()
         
+        for evento, pais, ano in evento_pais_ano_ordenados:
+            escritor.writerow({
+                'NOME-DO-EVENTO': evento,
+                'PAIS-DO-EVENTO': pais,
+                'ANO-DO-EVENTO': ano,
+                'OCORRENCIAS-EVENTO-PAIS-ANO':
+                frequencia_evento_pais_ano[(evento, pais, ano)]
+            })
+            totalLinhas4 += 1
+                    
+            if totalLinhas4 % intervaloFlush == 0:
+                f4.flush()
 
     tempo_total = time.time() - inicio
     print(f' Tempo de execução: {tempo_total:.2f} s')
     print(f' Total de linhas lidas: {totalLinhas}')
-    print(f' Qntd de linhas conjunto 1: {totalLinhas1}')
-    print(f' Qntd de linhas conjunto 2: {totalLinhas2}')
-    print(f' Qntd de linhas conjunto 3: {totalLinhas3}')
+    print(f' Qntd de linhas conjunto EVENTO: {totalLinhas1}')
+    print(f' Qntd de linhas conjunto EVENTO-ANO: {totalLinhas2}')
+    print(f' Qntd de linhas conjunto EVENTO-CIDADE-ANO: {totalLinhas3}')
+    print(f' Qntd de linhas conjunto EVENTO-PAIS-ANO: {totalLinhas4}')
 
 if __name__ == '__main__':
     main()
